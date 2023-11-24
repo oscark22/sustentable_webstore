1c1adc3b35cedb11c5ac1d0ab73bdf83b5dc10cb
M1keyy <miguel.alcantar@cetys.edu.mx>
2023-11-23 17:29:16 -0800

    cart

---
+++ src/lib/productCard.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let product : Product = {
+        id: "", name: "", price: 0
+    };
+
+    console.log(product);
+</script>
+
+<div class="card">
+    <header class="card-header">
+        <h2>Nombre del producto</h2>
+    </header>
+    <div class="card-body px-4">
+        Cantidad: <strong>4</strong>
+    </div>
+    <div class="card-body px-4">
+        Price : $10
+    </div>
+    <footer class="card-footer">
+        <button class="p-2 rounded variant-glass-primary">Añadir</button>
+        <button class="p-2 rounded variant-glass-error">Añadir</button>
+    </footer>
+</div>
